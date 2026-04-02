77a7c1a47012c0c2d747939b7dabda1b3eee71f7
Xuacha <68921033+Xuacha@users.noreply.github.com>
2026-04-02 15:37:32 -0500

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "7.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.8.21" apply false
+}
